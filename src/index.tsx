cf6f4a1385f06a8ac6a1cca66105d95e0fbf0963
Arleen Liu <arleenl@meta.com>
2025-01-05 03:15:46 -0800

    continuation of deploy #2

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,10 @@ import "antd/dist/antd.css";
 import Router from "./router";
 import i18n from "./translation";
 
+const basename = document.querySelector('base')?.getAttribute('href') ?? '/';
+
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <I18nextProvider i18n={i18n}>
       <Router />
     </I18nextProvider>
