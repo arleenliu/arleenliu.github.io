f4511db2ea2883eb687466e6b83496fe9d4fc3e8
Arleen Liu <arleenliu@gmail.com>
2026-05-13 03:39:32 -0400

    Updates

---
+++ static/js/566.47d2bd50.chunk.js
@@ -0,0 +1,45 @@
+"use strict";(globalThis.webpackChunkmy_app=globalThis.webpackChunkmy_app||[]).push([[566],{6566(e,i,t){t.r(i),t.d(i,{default:()=>b});var n=t(5043),r=t(4647),s=t(1319),d=t(3614),o=t(9281),c=t(5639),l=t(2646),a=t(9);const h=(0,a.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,p=(0,a.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,m=(0,a.Ay)(s.A)`
+  flex-direction: ${e=>{let{direction:i}=e;return"left"===i?"row":"row-reverse"}};
+`,x=(0,a.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,j=(0,a.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,y=(0,a.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,w=(0,a.Ay)("p")`
+  font-size: 13px;
+`,f=(0,a.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 30%;
+  }
+`;var g=t(579);const u=e=>{let{icon:i,title:t,content:n,section:o,button:a,t:u,id:b,direction:A}=e;return(0,g.jsx)(h,{children:(0,g.jsx)(d.zW,{direction:A,triggerOnce:!0,children:(0,g.jsxs)(m,{justify:"space-between",align:"middle",id:b,direction:A,children:[(0,g.jsx)(r.A,{lg:11,md:11,sm:12,xs:24,children:(0,g.jsx)(l.A,{src:i,width:"100%",height:"100%"})}),(0,g.jsx)(r.A,{lg:11,md:11,sm:11,xs:24,children:(0,g.jsxs)(x,{children:[(0,g.jsx)("h6",{children:u(t)}),(0,g.jsx)(p,{children:u(n)}),"right"===A?(0,g.jsx)(f,{children:"object"===typeof a&&a.map(((e,i)=>(0,g.jsx)(c.$,{color:e.color,onClick:()=>e.toLink?window.open(e.toLink,"_blank","noopener,noreferrer"):(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})("about"),children:u(e.title)},i)))}):(0,g.jsx)(j,{children:(0,g.jsx)(s.A,{justify:"space-between",children:"object"===typeof o&&o.map(((e,i)=>(0,g.jsxs)(r.A,{span:11,children:[(0,g.jsx)(l.A,{src:e.icon,width:"60px",height:"60px"}),(0,g.jsx)(y,{children:u(e.title)}),(0,g.jsx)(w,{children:u(e.content)})]},i)))})})]})})]})})})},b=(0,o.C)()((0,n.memo)(u))}}]);
+//# sourceMappingURL=566.47d2bd50.chunk.js.map
